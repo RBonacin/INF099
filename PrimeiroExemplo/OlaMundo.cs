033da80f0663b32275e4e954fc45cc1090dad545
Rodrigo Bonacin <30866458+RBonacin@users.noreply.github.com>
2023-05-25 21:20:02 -0300

    Add files via upload

---
+++ PrimeiroExemplo/OlaMundo.cs
@@ -0,0 +1,11 @@
+namespace INF099
+{
+    public class OlaMundo
+    {
+        static void Main(string[] args) {
+			// Comenario Novo
+            Console.WriteLine("Olá Alunos INF-0991 !!!!");
+			Console.WriteLine("Boa Noite !!!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# INF-099 - Controle de Versão 
+Curso de Extensão Tecnologias Microsoft - IC - UNICAMP
+Este repositório tem fins didáricos para referida disciplina.
+Professor Rodrigo Bonacin
+Maio 2023
